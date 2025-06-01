f77282c9902a9d19c47fa6e3a25e6a7a1e15023b
PedroRenatoES <pedrorenatoes@gmail.com>
2025-06-01 17:06:26 -0400

    Deploy23

--- sql/controllers/inventarioController.js
+++ sql/controllers/inventarioController.js
@@ -55,8 +55,16 @@ class InventarioController {
     }
   }
 
-  
-
+  static async getStockPorEstanteId(req, res) {
+    const { id_estante } = req.params;
+    try {
+      const datos = await DonacionesEnEspecieModel.getStockPorEstanteId(id_estante);
+      res.json(datos);
+    } catch (err) {
+      console.error('Error en getStockPorEstanteId:', err);
+      res.status(500).send('Error interno del servidor');
+    }
+  }
   
 }
 
