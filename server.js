cb88f2a83afbc0d5f3675d1f6fca36b94d04e0a7
PedroRenatoES <pedrorenatoes@gmail.com>
2025-06-18 08:07:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const uploadRoutes = require('./mongo/routes/uploadRoutes.js');
 const PORT = 3000;
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001', 'https://front-donaciones.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
